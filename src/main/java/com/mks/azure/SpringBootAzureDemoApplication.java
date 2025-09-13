1486e2b6718460260ee489c9035b90fe6ee8ad42
bhardwaj2000 <1710manish2000@gmail.com>
2025-09-13 17:14:40 +0530

    azure initial commit

---
+++ src/main/java/com/mks/azure/SpringBootAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mks.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mks/azure/controller/AzureDemoController.java
@@ -0,0 +1,16 @@
+package com.mks.azure.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AzureDemoController {
+
+    @GetMapping("/deploy/azure")
+    public ResponseEntity<String> getResponse(){
+        return ResponseEntity.ok("congratulations \uD83C\uDF89 you have deploy app on azure");
+    }
+}
